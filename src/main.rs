d4397de04f045a627dcc832b806eabde939ec2fe
oriabukha <alerya@hotmail.com>
2024-03-04 10:40:07 +0100

    Deserialization.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::{io::Error, net::TcpListener};
 use zero2prod::run;
 
+const ADDRESS: &str = "127.0.0.1";
+const PORT: u16 = 8000;
+
 #[tokio::main]
 pub async fn main() -> Result<(), Error> {
-    let listener = TcpListener::bind("127.0.0.1:8000").expect("Cannot bind to 8000 listener");
+    let address = format!("{}:{}", ADDRESS, PORT);
+    let listener = TcpListener::bind(address).expect("Cannot bind to 8000 listener");
     let _ = run(listener)?.await;
     Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+use reqwest::Client;
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a random port");
     let port = listener.local_addr().unwrap().port();
@@ -10,10 +12,16 @@ fn spawn_app() -> String {
     format!("http://127.0.0.1:{}", port)
 }
 
-#[tokio::test]
-async fn health_check_works() {
+fn make_client() -> (String, Client) {
     let address = spawn_app();
     let client = reqwest::Client::new();
+
+    (address, client)
+}
+
+#[tokio::test]
+async fn health_check_works() {
+    let (address, client) = make_client();
 
     let response = client
         .get(&format!("{}/health_check", &address))
@@ -23,4 +31,47 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length())
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let (address, client) = make_client();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let (address, client) = make_client();
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
 }
