89cfc6b112037d46325b24d97d661294730988ee
oriabukha <alerya@hotmail.com>
2024-03-09 07:25:39 +0100

    logging,

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes;
-use actix_web::{dev::Server, web, App, HttpRequest, HttpServer, Responder};
+use actix_web::{dev::Server, middleware, web, App, HttpRequest, HttpServer, Responder};
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -9,10 +10,13 @@ async fn greet(req: HttpRequest) -> impl Responder {
 }
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let db_pool = web::Data::new(db_pool);
 
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(middleware::Logger::default())
             .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(routes::health_check))
             .route("/subscriptions", web::post().to(routes::subscribe))
